9ca9d51999a46d7e6baa01ca42c0b931cf1bc7c7
superosrodki-ai <superosrodki@gmail.com>
2025-10-25 15:56:12 +0200

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
 // app/layout.js
 export const metadata = { title: 'CRM SuperObozy' };
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pl">
-      <body>{children}</body>
-    </html>
-  );
+  return (<html lang="pl"><body>{children}</body></html>);
 }
